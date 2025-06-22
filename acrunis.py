08270369bf6e8282cb3c3fb8636b5050e6a6bff7
Sys Acrunis 1o1 <114465675+Pentium-processor@users.noreply.github.com>
2025-06-22 21:42:55 +0100

    Delete acrunis.py

--- acrunis.py
+++
@@ -1,9 +0,0 @@
-import time 
-def counter(start,end):
-    numbers=[x for x in range(start,end+1,1)]
-    for num in numbers:
-        print(num,end='\r')
-        time.sleep(0.1)
-        
-if __name__=="__main__":
-    counter(1,100)
